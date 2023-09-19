f4a27600385c1bd85f62157618860e3220a3a1e4
Ana Victoria <avsotomejia@gmail.com>
2023-09-18 19:58:56 -0500

    Update

--- file.js
+++ file.js
@@ -1,17 +1,30 @@
 window.onload=function(){
     const toggleBtn = document.querySelector('.toggle_btn')
     const toggleBtnIcon = document.querySelector('.toggle_btn i')
-            const dropDownMenu = document.querySelector('.dropdown_menu')
+            const dropDownMenu = document.querySelector('.sidebar')
     
             toggleBtn.onclick = function(){
-                dropDownMenu.classList.toggle('open')
-                const isOpen = dropDownMenu.classList.contains('open')
+                dropDownMenu.classList.toggle('active')
+                const isOpen = dropDownMenu.classList.contains('active')
     
-                toggleBtnIcon.classList = isOpen
+              /*  toggleBtnIcon.classList = isOpen
                     ? 'fa-solid fa-xmark'
-                    : 'fa-solid fa-bars'
+                    : 'fa-solid fa-bars'*/
             } 
-  }
+    const btnCloseIcon = document.querySelector('.btn_close i')
+    
+    btnCloseIcon.onclick = function(){
+        dropDownMenu.classList.remove('active')
+    }
+}
+
+
+
+
+
+
+  
+
 
 
 
