132dcba5921566219ee29f8917b8c460fb37e954
avsotomejia@gmail.com <avsotomejia@gmail.com>
2023-10-17 21:38:48 -0500

    Update

--- listOfTypes.js
+++ listOfTypes.js
@@ -19,15 +19,47 @@ var dataCategories;
 function fillList(data){
     dataCategories = data;
     $(data).each(function(index,value){
-    var list = "<li>";
-    list += "<a class='title-description'>"+value.name+"</a>";
-    list += " <span classs='separator'>|</span>";
+    var list = "<li>"
+    list += "<a class='title-description'>"+value.name+"</a>"
+    list += " <span classs='separator'>|</span>"
     list += "</li>";
     $("#tbody-types").append(list);
     });
-    $("#tbody-types li").click(function(){
+
+    $("#tbody-types li a").each(function(index) {
+        $(this).attr("data-idtype", dataCategories[index].idType);
+    });
+
+    $("#tbody-types li a").click(function(){
+         var $a = $(this);
+        var idType = $a.data("idtype");
+
+        readProductsByTypes(idType);
+    
     })
 }
+
+function readProductsByTypes(idType){
+    var serviceRoute = "http://localhost/sokuservicio/productosportipo.php";
+
+    //I want to send a variable to the server
+    var formData = new FormData();
+    formData.append("caty", idType);
+
+    fetch(serviceRoute,
+        {
+            method: "POST",
+            body: formData
+        })
+                .then(function(response){
+                    return response.json();
+                })
+                .then(function(data){
+                    console.log(data);
+                })
+                
+}
+
 
 
 
